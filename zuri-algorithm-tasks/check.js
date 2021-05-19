e0ff8b3a31b868b8b6ba82753323d2fe715014ed
Abdulkareem <idayatolaide0@gmail.com>
2021-05-19 16:29:43 +0100

    update

--- zuri-algorithm-tasks/check.js
+++ zuri-algorithm-tasks/check.js
@@ -4,41 +4,32 @@ function checkYuGiOh(n){
      const result = [];
     let store = "";
     for(let currentValue = 1; currentValue <= n; currentValue++){
-        if(currentValue % 2 === 0 && currentValue %3==0 && currentValue %5 ==0 ) {
-            store = "yu-gi-oh";
-             result.push(store);
-        }
-        else if(currentValue % 2 === 0 && currentValue %5==0){
-            store = "yu-oh";
-             result.push(store);
-        }
-        else if(currentValue % 2 === 0 && currentValue %3==0){
-            store = "yu-gi";
-             result.push(store);
-        }
-        else if(currentValue % 2 === 0){
-            store = "yu";
-             result.push(store);
-        }
-         else if (currentValue % 3 === 0) {
-           store = "gi";
-           result.push(store);
-       } else if (currentValue % 5 === 0) {
-           store = "oh";
-           result.push(store);
-       } else {
-           result.push(currentValue);
-         }
+      if (
+        currentValue % 2 === 0 &&
+        currentValue % 3 == 0 &&
+        currentValue % 5 == 0
+      ) {
+        store = "yu-gi-oh";
+        result.push(store);
+      } else if (currentValue % 2 === 0 && currentValue % 5 == 0) {
+        store = "yu-oh";
+        result.push(store);
+      } else if (currentValue % 2 === 0 && currentValue % 3 == 0) {
+        store = "yu-gi";
+        result.push(store);
+      } else if (currentValue % 2 === 0) {
+        store = "yu";
+        result.push(store);
+      } else if (currentValue % 3 === 0) {
+        store = "gi";
+        result.push(store);
+      } else if (currentValue % 5 === 0) {
+        store = "oh";
+        result.push(store);
+      } else {
+        result.push(currentValue);
+      }
     }
-
-
-//     if (store){
-//    result.push(store);
-//     }
-// else {
-  // result.push(currentValue);
-
-
 return result
 }
 
